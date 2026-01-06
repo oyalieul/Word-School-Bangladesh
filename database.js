8eaa0d45877f9c2c59498b4a0c979f35b12eb64c
oyalieul <contactoyalieul@gmail.com>
2026-01-06 11:00:55 +0600

    first commit

---
+++ database.js
@@ -0,0 +1,54 @@
+/**
+ * WordSchoolBangladesh Vocabulary Database
+ * * Structure:
+ * "Class Number": {
+ * "Unit Number": {
+ * "Lesson Number": [
+ * { en: "Word", syn: "Definition/Synonym", bn: "Bangla", ex: "Example Sentence" }
+ * ]
+ * }
+ * }
+ */
+
+const vocabDatabase = {
+    "6": {
+        "Unit 1": {
+            "Lesson 1": [
+                { en: "complete", syn: "finished; having all parts", bn: "সম্পূর্ণ", ex: "Please complete the lesson." },
+                { en: "familiar", syn: "well known", bn: "পরিচিত", ex: "This place looks familiar to me." },
+                { en: "familiar objects", syn: "things we already know", bn: "পরিচিত জিনিসপত্র", ex: "We learn using familiar objects." },
+                { en: "fill out", syn: "to write information in a form", bn: "ফরম পূরণ করা", ex: "Fill out the form carefully." },
+                { en: "hobby", syn: "something you like to do in free time", bn: "শখ", ex: "Reading is my hobby." },
+                { en: "comic book", syn: "a book with pictures and stories", bn: "কমিক বই", ex: "He likes reading comic books." },
+                { en: "similar", syn: "almost the same", bn: "মিল আছে এমন", ex: "The two pictures are similar." },
+                { en: "understand", syn: "to know the meaning", bn: "বুঝতে পারা", ex: "I understand the lesson." },
+                { en: "reach", syn: "to arrive at a place", bn: "পৌঁছানো", ex: "We reached school early." },
+                { en: "seat", syn: "a place to sit", bn: "আসন", ex: "Please take your seat." },
+                { en: "greet", syn: "to say hello", bn: "শুভেচ্ছা জানানো", ex: "She greeted her teacher." },
+                { en: "frightened", syn: "feeling fear", bn: "ভীত", ex: "The child was frightened." },
+                { en: "excited", syn: "very happy and eager", bn: "খুব আনন্দিত", ex: "I am excited about the trip." },
+                { en: "warmly", syn: "in a kind way", bn: "আন্তরিকভাবে", ex: "She welcomed us warmly." },
+                { en: "amusing", syn: "funny and enjoyable", bn: "মজার", ex: "The story was amusing." },
+                { en: "afraid", syn: "feeling fear", bn: "ভয় পাওয়া", ex: "He is afraid of dogs." },
+                { en: "glad", syn: "happy and pleased", bn: "খুশি", ex: "I am glad to see you." },
+                { en: "thrilled", syn: "very excited", bn: "খুব আনন্দিত", ex: "She was thrilled to win." },
+                { en: "shy", syn: "feeling nervous with people", bn: "লাজুক", ex: "He is a shy boy." },
+                { en: "laughter", syn: "the sound of laughing", bn: "হাসি", ex: "The room was full of laughter." },
+                { en: "loudest", syn: "having the most noise", bn: "সবচেয়ে জোরে", ex: "He shouted the loudest." },
+                { en: "poem", syn: "a piece of writing with rhythm", bn: "কবিতা", ex: "She wrote a poem." },
+                { en: "lesson", syn: "a part of a subject taught in school", bn: "পাঠ", ex: "We learned a new lesson today." },
+                { en: "objects", syn: "things that you can see or touch", bn: "জিনিসপত্র", ex: "The classroom has many objects." },
+                { en: "form", syn: "a paper to fill information", bn: "ফরম", ex: "Fill out the registration form." },
+                { en: "after", syn: "following something in time", bn: "পরে", ex: "Wash your hands after eating." },
+                { en: "know", syn: "to have information or understanding", bn: "জানা", ex: "I know the answer." },
+                { en: "parents", syn: "mother and father", bn: "পিতা-মাতা", ex: "My parents are very kind." },
+                { en: "peanuts", syn: "small edible nuts", bn: "চিনা বাদাম", ex: "I like roasted peanuts." },
+                { en: "found", syn: "discovered or saw something", bn: "খুঁজে পাওয়া", ex: "I found my pen under the table." },
+                { en: "went", syn: "past of “go”; moved somewhere", bn: "গিয়েছিল", ex: "He went to the market." },
+                { en: "felt", syn: "past of “feel”; experienced", bn: "অনুভব করা", ex: "I felt happy yesterday." },
+                { en: "begun", syn: "past participle of “begin”; started", bn: "শুরু হয়েছে", ex: "The game has just begun." }
+            ]
+        }
+    }
+    // You can add Class 7, 8 etc here later.
+};
